4bbeb4416b261af45277e62b85878e9291fee10f
Vu <vudang30112002@gmail.com>
2024-10-20 22:00:36 +0700

    .

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = "Em";
+const giftUrl = "https://www.facebook.com/victor.dangz";
+const eventName = "Chúc Mừng 20-10";
+const titleCard = "Nguyễn Hồng Phương";
+const contentCard =
+  "Anh Vũ ông trùm kéo view số 1 Việt Nam chúc em iu của anh ngày 20/10 thật vui vẻ và hạnh phúc, tràn đầy sức khoẻ, gia đình luôn luôn bình an, tài lộc đồng hành, thần tài dẫn lối, 8386 mãi đỉnh, mãi đỉnh";
+
+const giftImage = "punyone.jpg";
+const base64 = "";
+const giftImageBase64 = "data:image/png;base64, " + base64;
